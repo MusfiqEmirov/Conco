7c41b8b544ec1cb9041fe58927e4cf39e6eb82c4
Ebil Abilli <ebilebilli3gmail.com>
2026-01-10 20:11:33 +0400

    django project and models created

---
+++ conco/projects/admin/admin_v1.py
@@ -0,0 +1,131 @@
+from django.contrib import admin
+from projects.models import *
+
+# Media 
+@admin.register(Media)
+class MediaAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'about',
+        'project',
+        'partner',
+        'vacancy',
+        'is_home_page_background_image',
+        'is_about_page_background_image',
+        'is_partner_background_image',
+        'is_project_page_background_image',
+        'created_at',
+    )
+    list_filter = (
+        'is_home_page_background_image',
+        'is_about_page_background_image',
+        'is_partner_background_image',
+        'is_project_page_background_image',
+        'created_at',
+    )
+    readonly_fields = ('created_at',)
+
+
+class MediaInlineBase(admin.TabularInline):
+    model = Media
+    extra = 1
+    readonly_fields = ('created_at',)
+    fields = (
+        'image',
+    )
+
+
+class MediaInlineProject(MediaInlineBase):
+    fields = MediaInlineBase.fields + ('video',)
+
+
+class MediaInlinePartner(MediaInlineBase):
+    pass
+
+
+class MediaInlineAbout(MediaInlineBase):
+    pass
+
+# Project Category 
+@admin.register(ProjectCategory)
+class ProjectCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name_az',)
+    search_fields = ('name_az', 'name_en', 'name_ru')
+
+# Project 
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'name_az',
+        'category',
+        'is_completed',
+        'is_active',
+        'created_at',
+    )
+    list_filter = (
+        'category',
+        'is_completed',
+        'is_active',
+        'created_at',
+    )
+    search_fields = ('name_az', 'name_en', 'name_ru', 'description_az', 'description_en', 'description_ru')
+    exclude = ('slug',)
+    inlines = [MediaInlineProject]
+
+# Partner 
+@admin.register(Partner)
+class PartnerAdmin(admin.ModelAdmin):
+    list_display = (
+        'name_az',
+        'url',
+        'is_active',
+        'created_at',
+    )
+    list_filter = ('is_active', 'created_at')
+    search_fields = ('name_az', 'name_en', 'name_ru')
+    ordering = ('-created_at',)
+    inlines = [MediaInlinePartner]
+
+# About 
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+    list_display = ('main_title_az', 'second_title_az')
+    search_fields = ('main_title_az', 'second_title_az', 'description_az')
+    inlines = [MediaInlineAbout]
+
+# Contact 
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        'address_az',
+        'phone',
+        'email',
+    )
+    search_fields = (
+        'address_az',
+        'phone',
+        'email',
+    )
+
+# Vacancy 
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = (
+        'title_az',
+        'is_active',
+        'created_at',
+    )
+    list_filter = ('is_active', 'created_at')
+    search_fields = ('title_az', 'title_en', 'title_ru', 'description_az', 'description_en', 'description_ru')
+
+# Appeal (CV) 
+@admin.register(Appeal)
+class AppealAdmin(admin.ModelAdmin):
+    list_display = (
+        'vacancy',
+        'cv',
+        'is_read',
+        'created_at',
+    )
+    list_filter = ('is_read', 'created_at')
+    readonly_fields = ('created_at',)
---
+++ conco/projects/utils/__init__.py
@@ -0,0 +1,6 @@
+from .abstract_models import SluggedModel
+from .unique_slugify import unique_slugify
+from .normalize_phone_number import normalize_az_phone
+
+
+__all__ = ['SluggedModel', 'unique_slugify', 'normalize_az_phone',]
